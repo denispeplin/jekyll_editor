ffdb112ea132cd7e95b11143ac6fd0666c52190d
denispeplin <denis.peplin@gmail.com>
2016-08-17 19:50:41 +0300

    Test for errors, when no folder or no _posts in folder.

--- test/jekyll_editor/index_test.exs
+++ test/jekyll_editor/index_test.exs
@@ -2,8 +2,18 @@ defmodule JekyllEditor.IndexTest do
   use ExUnit.Case
   doctest JekyllEditor.Index
 
-  test "#call" do
-    assert JekyllEditor.Index.call("test/source") ==
-      {:ok, ["2016-08-14-welcome-to-jekyll.markdown", "2016-08-15-new-article.markdown"]}
+  describe "#call" do
+    test "path points to proper Jekyll site" do
+      assert JekyllEditor.Index.call("test/source") ==
+        {:ok, ["2016-08-14-welcome-to-jekyll.markdown", "2016-08-15-new-article.markdown"]}
+    end
+
+    test "path points to folder without _posts" do
+      assert JekyllEditor.Index.call("test") == {:error, :enoent}
+    end
+
+    test "path points to nonexistent folder" do
+      assert JekyllEditor.Index.call("nonexistent") == {:error, :enoent}
+    end
   end
 end
